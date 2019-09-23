5eb9e3d5c2b6ac76085d9ac428dba99ed74f0073
thec33 <416e306e@gmail.com>
2019-09-23 21:39:17 +0800

    Initial commit

---
+++ buleplanet/Assets/Scripts/Notes/noteSpawn.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class noteSpawn : MonoBehaviour
+{
+    public GameObject note;
+    public int spawnSide = 5;
+    public float noteSpeed = 10f;
+    public Transform target;
+    public bool isMoving = true;
+    public GameObject clone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+
+        transform.LookAt(target);//look at the BULE planet
+
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            generateNote();
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+        if (isMoving == true)
+        {
+            moveNote();
+        }
+    }
+
+    void generateNote()
+    {
+        clone = Object.Instantiate(note, transform.position, transform.rotation); //instantiate note and assign it to clone
+    }
+    void moveNote()
+    {
+        clone.transform.Translate(Vector3.forward * noteSpeed * Time.deltaTime); // Slide the note towards BULE planet
+    }
+
+    private void OnTriggerEnter(Collider other) //detect shield hit
+    {
+        
+        Destroy(note); // destroy the note
+    }
+}
---
+++ buleplanet/Assets/Scripts/ShieldMovement.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    public GameObject shieldObject;
+    public bool useDefaultControls = true; //use default controls?
+    public KeyCode movementKey1;
+    public KeyCode movementKey2;
+    public KeyCode movementKey3;
+    public KeyCode movementKey4;
+    public KeyCode movementKey5;
+    public KeyCode movementKey6;
+    public int currentFace;
+    public Material highlightMaterial;
+
+
+    
+    // Start is called before the first frame update YEAH AND
+    void Start()
+    {
+        currentFace = 1;
+    }
+
+    // Update is called once per frame NOSH
+    void Update()
+    {
+        assignMovementKeys();
+        currentFaceUpdate();
+        applyMovement();
+    }
+
+    public void assignMovementKeys()
+    {
+        if(useDefaultControls)
+        {
+            movementKey1 = KeyCode.S;
+            movementKey2 = KeyCode.D;
+            movementKey3 = KeyCode.F;
+            movementKey4 = KeyCode.J;
+            movementKey5 = KeyCode.K;
+            movementKey6 = KeyCode.L;
+        }
+    }
+
+    public  void applyMovement()
+    {
+        switch(currentFace)
+        {
+            case 1:
+            
+            transform.position = new Vector3(8,0, 0);
+            transform.rotation = Quaternion.Euler(new Vector3(90, 90, 0));
+            break;
+
+            case 2:
+            transform.position = new Vector3(0, 8, 0);
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            break;
+
+            case 3:
+              transform.position = new Vector3(-8, 0, 0);
+            transform.rotation = Quaternion.Euler(new Vector3(90, 90, 0));
+            break;
+
+            case 4:
+            transform.position = new Vector3(0, -8, 0);
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            break;
+
+            case 5:
+            transform.position = new Vector3(0, 0, -8);
+            transform.rotation = Quaternion.Euler(new Vector3(90, 0, 0));
+            break;
+
+            case 6:
+            transform.position = new Vector3(0, 0, 8);
+            transform.rotation = Quaternion.Euler(new Vector3(90, 0, 0));
+            break;
+        }
+    }
+
+    public void currentFaceUpdate()
+    {
+        if(Input.GetKeyDown(movementKey1))
+        {
+            currentFace=1;
+        }
+        if(Input.GetKeyDown(movementKey2))
+        {
+            currentFace=2;
+        }
+        if(Input.GetKeyDown(movementKey3))
+        {
+            currentFace=3;
+        }
+        if(Input.GetKeyDown(movementKey4))
+        {
+            currentFace=4;
+        }
+        if(Input.GetKeyDown(movementKey5))
+        {
+            currentFace=5;
+        }
+        if(Input.GetKeyDown(movementKey6))
+        {
+            currentFace=6;
+        }
+    }
+
+    public void OnTriggerEnter()
+    {
+        shieldObject.GetComponent<MeshRenderer>().material = highlightMaterial;
+    }
+
+}
